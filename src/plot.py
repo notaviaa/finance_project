3d82cc618566206a14acc30f47c83fa6e899db28
Andy Nguyen <huuanhduyn@student.unimelb.edu.au>
2025-12-24 17:20:45 +0700

    24/12 end, graph plot update

--- src/plot.py
+++ src/plot.py
@@ -4,3 +4,6 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+def close_price(df: pd.DataFrame):
+    plt.plot(df['time'], df['close'])
+    plt.xaxis.set_major_formatter(date_format)
